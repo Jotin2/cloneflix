f597efb07c487748584044977ec5bcbaebe590dc
Joshua Asari <josh.asari@gmail.com>
2024-03-13 15:51:19 -0400

    "Initial Commit"

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000", // frontend URI (ReactJS)
+};
+app.use(express.json());
+app.use(cors(corsOptions));
 
-app.listen(port, () => {
-    console.log(`server is up and listening on ${port}`);
+// connect MongoDB
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => {
+            console.log(`App is Listening on PORT ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// route
+app.get("/", (req, res) => {
+    res.status(201).json({ message: "Connected to Backend!" });
 });
